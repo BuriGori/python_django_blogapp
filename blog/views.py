a1badf1b4b8f5c5428cde98e8e9992b0b3340fdc
bgr <jamasa97@icloud.com>
2022-02-15 13:12:18 +0900

    Add: post_edit.html 추가

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from blog.forms import PostModelForm
 from blog.models import Post
 
+
+def post_new(request):
+    if request.method=='POST':
+        post_form = PostModelForm(request.POST)
+        if post_form.is_valid():
+            post = form = post_form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        post = PostModelForm()
+    return render(request, 'blog/post_edit.html', {'form': post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
     # http_method = request.method
