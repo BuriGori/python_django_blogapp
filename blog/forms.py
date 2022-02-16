852b9d6163582e9bffe572281665eec5937b1bcf
bgr <jamasa97@icloud.com>
2022-02-16 11:39:33 +0900

    Add: Login 기능 추가

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,11 @@ class PostModelForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text',)
 
+def min_length_5_validator(value):
+    if len(value) < 5:
+        raise forms.ValidationError('text은 5글자 이상 입력해주세요')
+
+
+class PostForm(forms.Form):
+    title = forms.CharField()
+    text = forms.CharField(widget=forms.Textarea, validators=[min_length_5_validator])
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,58 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from blog.forms import PostModelForm
+from blog.forms import PostModelForm, PostForm
 from blog.models import Post
 
+def post_remove(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('post_list_home')
 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostModelForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'postform': form})
+
+@login_required
 def post_new(request):
-    if request.method=='POST':
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            clean_data_dict = form.cleaned_data
+            post = Post.objects.create(
+                author=request.user,
+                title=clean_data_dict.get('title'),
+                text=clean_data_dict.get('text'),
+                published_date=timezone.now()
+            )
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'postform': form})
+
+def post_new_modelform(request):
+    if request.method == 'POST':
         post_form = PostModelForm(request.POST)
         if post_form.is_valid():
-            post = form = post_form.save(commit=False)
+            post = post_form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        post = PostModelForm()
-    return render(request, 'blog/post_edit.html', {'form': post})
+        post_form = PostModelForm()
+    return render(request, 'blog/post_edit.html', {'form': post_form})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
